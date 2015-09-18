26f80ed0d0dd28a6b9382e7cb3c1144af43b2872
Ronny Kraemer <AvE@Prometheus.fritz.box>
2015-09-18 21:21:44 +0200

    Implement TwitterIntegration

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+require 'twitter'
+require_relative 'lib/twitter_integration'
+require_relative 'helpers/view_helper'
 
 get '/' do
 		'Hello, you!'
 end
 
-get '/:name' do
-		"Hello, #{params[:name]}!"
+get '/twitter' do
+	twitter = TwitterIntegration.new
+	user = ENV['TWITTER_USERNAME']
+	options = {:count => 50, :include_rts => true, :exclude_replies => true}
+
+	erb :'twitter.html', :locals => { :tweets => twitter.client.user_timeline(user, options), :helper => ViewHelper }
 end
