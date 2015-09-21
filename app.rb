f1a80a516d3e9dc831346cce5932478593b49b17
Ronny Kraemer <AvE@Prometheus.fritz.box>
2015-09-22 00:29:06 +0200

    Add and configure Logentries

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
 require 'twitter'
 require 'tzinfo'
+require 'sinatra-logentries'
 require_relative 'lib/twitter_integration'
 require_relative 'helpers/view_helper'
+
+configure { Sinatra::Logentries.token = ENV['LOGENTRIES_TOKEN'] }
+
+logger.info("I'm a Lumberjack and I'm OK")
 
 get '/' do
   twitter = TwitterIntegration.new
