36bcaf95f841f95fc499a28360a08c42451018fc
Ronny Kraemer <AvE@Prometheus.fritz.box>
2015-09-21 02:35:53 +0200

    Integrate Twitter implementation into my homepage design

--- app.rb
+++ app.rb
@@ -4,13 +4,9 @@ require_relative 'lib/twitter_integration'
 require_relative 'helpers/view_helper'
 
 get '/' do
-		'Hello, you!'
-end
+  twitter = TwitterIntegration.new
+  user = ENV['TWITTER_USERNAME']
+  options = {:count => 100, :include_rts => true, :exclude_replies => true}
 
-get '/twitter' do
-	twitter = TwitterIntegration.new
-	user = ENV['TWITTER_USERNAME']
-	options = {:count => 50, :include_rts => true, :exclude_replies => true}
-
-	erb :'twitter.html', :locals => { :tweets => twitter.client.user_timeline(user, options), :helper => ViewHelper }
+  erb :'home.html', :locals => {:tweets => twitter.client.user_timeline(user, options), :helper => ViewHelper}
 end
--- helpers/view_helper.rb
+++ helpers/view_helper.rb
@@ -8,6 +8,10 @@ module ViewHelper
       tweet.urls.collect { |url| [url.url, url.display_url] } if tweet.urls.respond_to?(:each)
     end
 
+    def collect_user_mentions(tweet)
+      tweet.user_mentions.collect { |user_mention| "@#{user_mention.screen_name}" } if tweet.user_mentions.respond_to?(:each)
+    end
+
     def generate_link(entities, tweet_text)
       replace_hash = {}
       entities.each do |entity|
@@ -15,7 +19,11 @@ module ViewHelper
             if entity[0].is_a?(Addressable::URI)
               "<a href='#{entity[0]}'>http://#{entity[1]}</a>"
             else
-              "<a href='https://twitter.com/hashtag/#{entity.delete('#')}'>#{entity}</a>"
+              if entity.start_with?('@')
+                "<a href='https://twitter.com/#{entity.delete('@')}'>#{entity}</a>"
+              else
+                "<a href='https://twitter.com/hashtag/#{entity.delete('#')}'>#{entity}</a>"
+              end
             end
       end
       replace_hash.each do |key, value|
@@ -23,5 +31,11 @@ module ViewHelper
       end
       return tweet_text
     end
+
+    def format(datetime)
+      format = '%d.%m.%Y, %H:%M Uhr'
+      datetime.to_datetime.strftime(format)
+    end
+
   end
 end
