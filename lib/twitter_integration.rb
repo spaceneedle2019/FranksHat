ecffcfe88c4f81d1372811639581b69ebfaab7e4
Ronny Kraemer <mail@rk82.net>
2016-12-04 16:33:23 +0100

    Rework Twitter integration

--- lib/twitter_integration.rb
+++ lib/twitter_integration.rb
@@ -1,18 +1,28 @@
 class TwitterIntegration
-  attr_accessor :client
+  attr_accessor :client, :user_name, :options
 
   def initialize
-    @client = configurate!
+    @client = authenticate
+    @user_name = set_user_name
+    @options = set_options
   end
 
   private
 
-  def configurate!
+  def authenticate
     Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
       config.access_token = ENV['TWITTER_ACCESS_TOKEN']
       config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
+  end
+
+  def set_user_name
+    ENV['TWITTER_USERNAME']
+  end
+
+  def set_options
+    {:count => 100, :include_rts => false, :exclude_replies => true}
   end
 end
