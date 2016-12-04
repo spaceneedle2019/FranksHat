1d02def336d63eb6548320dd41a3e1aeadad8155
Ronny Kraemer <mail@rk82.net>
2016-12-04 16:32:07 +0100

    Enhance app.rb for using Instagram integration

--- app.rb
+++ app.rb
@@ -2,17 +2,25 @@ require 'sinatra'
 require 'twitter'
 require 'tzinfo'
 require 'sinatra-logentries'
+require 'tilt/erb'
 require_relative 'lib/twitter_integration'
+require_relative 'lib/instagram_integration'
 require_relative 'helpers/view_helper'
+require_relative 'helpers/application_helper'
 
 configure { Sinatra::Logentries.token = ENV['LOGENTRIES_TOKEN'] }
 DEFAULT_LAYOUT = :'layouts/default.html'
 
 get '/' do
-  twitter = TwitterIntegration.new; user = ENV['TWITTER_USERNAME']
-  options = {:count => 100, :include_rts => false, :exclude_replies => true}
-  
-  erb :'home.html', :layout => DEFAULT_LAYOUT, :locals => {:tweets => twitter.client.user_timeline(user, options), :helper => ViewHelper}
+  twitter = TwitterIntegration.new
+  instagram = InstagramIntegration.new
+  timeline =
+      ApplicationHelper.merge_timelines(
+          twitter.client.user_timeline(twitter.user_name, twitter.options),
+          instagram.media
+      )
+  ApplicationHelper.remove_instagram_duplicates_from(timeline)
+  erb :'home.html', :layout => DEFAULT_LAYOUT, :locals => {:timeline => timeline, :helper => ViewHelper}
 end
 
 get '/:things' do
