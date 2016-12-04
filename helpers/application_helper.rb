4e33da88c93cbccb4af6d74e96815ea31940c346
Ronny Kraemer <mail@rk82.net>
2016-12-04 16:32:54 +0100

    Introduce ApplicationHelper

---
+++ helpers/application_helper.rb
@@ -0,0 +1,22 @@
+module ApplicationHelper
+  class << self
+    def merge_timelines(twitter, instagram)
+      timeline = twitter.concat(instagram)
+      timeline.map! { |entry| [convert(entry.created_at), entry] }.sort! {|a, b|  b[0] <=> a[0] }
+    end
+
+    def remove_instagram_duplicates_from(timeline)
+      timeline.each_with_index do |(_, entry), index|
+        timeline.delete_at(index) if entry.is_a?(Twitter::Tweet) && entry.source.include?('Instagram')
+      end
+      timeline
+    end
+
+    private
+
+    def convert(timestamp)
+      timezone = TZInfo::Timezone.get('Europe/Berlin')
+      timezone.utc_to_local(timestamp.to_datetime)
+    end
+  end
+end
