43dca24471eeacf5b3b6d2aa8e1379eeec662a26
Ronny Kraemer <mail@rk82.net>
2017-02-06 21:52:25 +0100

    Refactor app.rb
    * Use class logic
    * Use helpers
    * Use before hook
    * Add client and server error handling

--- app.rb
+++ app.rb
@@ -3,32 +3,50 @@ require 'twitter'
 require 'tzinfo'
 require 'sinatra-logentries'
 require 'tilt/erb'
-require 'dotenv' if Sinatra::Base.development?
+require_relative 'helpers/application_helper'
+require_relative 'helpers/view_helper'
 require_relative 'lib/twitter_integration'
 require_relative 'lib/instagram_integration'
-require_relative 'helpers/view_helper'
-require_relative 'helpers/application_helper'
 
-Dotenv.load('.env') if Sinatra::Base.development?
 configure { Sinatra::Logentries.token = ENV['LOGENTRIES_TOKEN'] }
-DEFAULT_LAYOUT = :'layouts/default.html'
 
-get '/' do
-  twitter = TwitterIntegration.new
-  instagram = InstagramIntegration.new
-  timeline =
-      ApplicationHelper.merge_timelines(
-          twitter.client.user_timeline(twitter.user_name, twitter.options),
-          instagram.media
-      )
-  ApplicationHelper.remove_instagram_duplicates!(timeline)
-  erb :'home.html', :layout => DEFAULT_LAYOUT, :locals => {:timeline => timeline, :helper => ViewHelper}
+class App < Sinatra::Base
+  if App.development?
+    require 'dotenv'
+    Dotenv.load('.env')
+  end
+
+  before do
+    DEFAULT_LAYOUT = :'layouts/default.html'.freeze
+  end
+
+  get '/' do
+    erb :'home.html', layout: DEFAULT_LAYOUT, locals: {timeline: build_timeline, helper: ViewHelper}
+  end
+
+  get '/:things' do
+    erb :'not_found.html', layout: DEFAULT_LAYOUT, locals: {things: params[:things]}
+  end
+
+  not_found do
+    erb :'not_found.html', layout: DEFAULT_LAYOUT
+  end
+
+  error 400..511 do
+    erb :'error.html', layout: DEFAULT_LAYOUT, locals: {status: response.status}
+  end
+
+  helpers do
+    def build_timeline
+      twitter = TwitterIntegration.new
+      instagram = InstagramIntegration.new
+      timeline =
+          ApplicationHelper.merge_timelines(
+              twitter.client.user_timeline(twitter.user_name, twitter.options),
+              instagram.media
+          )
+      ApplicationHelper.remove_instagram_duplicates!(timeline)
+    end
+  end
 end
 
-get '/:things' do
-  erb :'not_found.html', :layout => DEFAULT_LAYOUT, :locals => {:things => params[:things]}
-end
-
-not_found do
-  erb :'not_found.html', :layout => DEFAULT_LAYOUT
-end
