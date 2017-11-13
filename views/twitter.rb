b529404198e6e2c2b3636d7bba53bd3980fdb271
Ronny Kraemer <mail@rk82.net>
2017-11-14 00:42:20 +0100

    Refactor Views:Twitter methods

--- views/twitter.rb
+++ views/twitter.rb
@@ -4,38 +4,32 @@ module Views
   module Twitter
     class << self
       def parse(tweet)
-        user_mentions = collect_user_mentions(tweet)
-        hashtags = collect_hashtags(tweet)
-        urls = collect_short_urls(tweet)
+        hashtags, urls, user_mentions = collect_data(tweet)
         tweet_text = tweet.text.dup
-        generate_link(hashtags, tweet_text) unless hashtags.count == 0
-        generate_link(user_mentions, tweet_text) unless user_mentions.count == 0
-        generate_link(urls, tweet_text) unless urls.count == 0
-        generate_link_for_unmatched_urls(tweet_text)
-        remove_fb_hashtag_from(tweet_text)
+        generate_link!(tweet_text, hashtags) unless hashtags.count == 0
+        generate_link!(tweet_text, user_mentions) unless user_mentions.count == 0
+        generate_link!(tweet_text, urls) unless urls.count == 0
+        generate_link_for_unmatched_urls!(tweet_text)
+        remove_fb_hashtag!(tweet_text)
       end
 
       private
 
-      def collect_hashtags(tweet)
-        tweet.hashtags.collect {|key| "#{Views::HASHMARK}#{key.text}" unless key.text == 'fb'}.compact
+      def collect_data(tweet)
+        hashtags = tweet.hashtags.collect {|key| "#{Views::HASHMARK}#{key.text}" unless key.text == 'fb'}.compact
+        short_urls = tweet.urls.collect {|url| [url.url, url.display_url]} if tweet.urls.respond_to?(:each)
+        user_mentions =
+            if tweet.user_mentions.respond_to?(:each)
+              tweet.user_mentions.collect {|user_mention| "#{Views::AT_SIGN}#{user_mention.screen_name}"}
+            end
+        return hashtags, short_urls, user_mentions
       end
 
-      def collect_short_urls(tweet)
-        tweet.urls.collect {|url| [url.url, url.display_url]} if tweet.urls.respond_to?(:each)
-      end
-
-      def collect_user_mentions(tweet)
-        if tweet.user_mentions.respond_to?(:each)
-          tweet.user_mentions.collect {|user_mention| "#{Views::AT_SIGN}#{user_mention.screen_name}"}
-        end
-      end
-
-      def remove_fb_hashtag_from(tweet_text)
+      def remove_fb_hashtag!(tweet_text)
         tweet_text.include?('#fb') ? tweet_text.gsub!(/#fb/, '').strip : tweet_text
       end
 
-      def generate_link_for_unmatched_urls(tweet_text)
+      def generate_link_for_unmatched_urls!(tweet_text)
         if tweet_text.match(Views::URL_REGEX)
           tweet_text.gsub!(Views::URL_REGEX) {|m| " <a href='#{m.lstrip}'>#{m.lstrip}</a>"}
         else
@@ -43,24 +37,32 @@ module Views
         end
       end
 
-      def generate_link(entities, tweet_text)
-        replace_hash = {}
-        entities.each do |entity|
-          replace_hash[entity] =
-              if entity[0].is_a?(Addressable::URI)
-                "<a href='#{entity[0]}'>http://#{entity[1]}</a>"
-              else
-                if entity.start_with?(Views::AT_SIGN)
-                  "<a href='https://twitter.com/#{entity.delete(Views::AT_SIGN)}'>#{entity}</a>"
-                else
-                  "<a href='https://twitter.com/hashtag/#{entity.delete(Views::HASHMARK)}'>#{entity}</a>"
-                end
-              end
-        end
-        replace_hash.each do |key, value|
-          key.is_a?(Array) ? tweet_text.sub!(key[0], value) : tweet_text.sub!(key, value)
-        end
+      def generate_link!(tweet_text, entities)
+        hash =
+            entities.each_with_object({}) do |entity, hash|
+              hash[entity] =
+                  if entity[0].is_a?(Addressable::URI)
+                    "<a href='#{entity[0]}'>http://#{entity[1]}</a>"
+                  else
+                    if entity.start_with?(Views::AT_SIGN)
+                      "<a href='https://twitter.com/#{entity.delete(Views::AT_SIGN)}'>#{entity}</a>"
+                    else
+                      "<a href='https://twitter.com/hashtag/#{entity.delete(Views::HASHMARK)}'>#{entity}</a>"
+                    end
+                  end
+            end
+        search_and_replace!(hash, tweet_text)
         tweet_text
+      end
+
+      def search_and_replace!(hash, tweet_text)
+        hash.each do |key, value|
+          if key.is_a?(Array)
+            tweet_text.sub!(key[0], value)
+          else
+            tweet_text.sub!(key, value) || tweet_text.sub!(key.downcase, value)
+          end
+        end
       end
     end
   end
