49033658287131138d42fa927887f36de9704127
Ronny Kraemer <mail@rk82.net>
2017-11-13 23:09:14 +0100

    Change requirements and use application „helpers“

--- app.rb
+++ app.rb
@@ -3,15 +3,14 @@ require 'twitter'
 require 'tzinfo'
 require 'sinatra-logentries'
 require 'tilt/erb'
-require_relative 'helpers/application_helper'
-require_relative 'helpers/view_helper'
 require_relative 'lib/twitter_integration'
 require_relative 'lib/instagram_integration'
+require_relative 'views/base'
 
 configure { Sinatra::Logentries.token = ENV['LOGENTRIES_TOKEN'] }
 
 class App < Sinatra::Base
-  DEFAULT_LAYOUT = :'layouts/default.html'.freeze
+  DEFAULT_LAYOUT = :'templates/layouts/default.html'.freeze
 
   if App.development?
     require 'dotenv'
@@ -19,31 +18,44 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    erb :'home.html', layout: DEFAULT_LAYOUT, locals: {timeline: build_timeline, helper: ViewHelper}
+    erb :'templates/home.html', layout: DEFAULT_LAYOUT,
+        locals: {timeline: build_timeline, base: Views::Base, instagram: Views::Instagram, twitter: Views::Twitter}
   end
 
   get '/:things' do
-    erb :'not_found.html', layout: DEFAULT_LAYOUT, locals: {things: params[:things]}
+    erb :'templates/not_found.html', layout: DEFAULT_LAYOUT, locals: {things: params[:things]}
   end
 
   not_found do
-    erb :'not_found.html', layout: DEFAULT_LAYOUT
+    erb :'templates/not_found.html', layout: DEFAULT_LAYOUT
   end
 
   error 400..511 do
-    erb :'error.html', layout: DEFAULT_LAYOUT, locals: {status: response.status}
+    erb :'templates/error.html', layout: DEFAULT_LAYOUT, locals: {status: response.status}
   end
 
   helpers do
     def build_timeline
       twitter = TwitterIntegration.new
       instagram = InstagramIntegration.new
-      timeline =
-          ApplicationHelper.merge_timelines(
-              twitter.client.user_timeline(twitter.user_name, twitter.options),
-              instagram.media
-          )
-      ApplicationHelper.remove_instagram_duplicates!(timeline)
+      timeline = merge_timelines(twitter.client.user_timeline(twitter.user_name, twitter.options), instagram.media)
+      remove_instagram_duplicates!(timeline)
+    end
+
+    def merge_timelines(twitter, instagram)
+      timeline = twitter.concat(instagram)
+      timeline.map! { |entry| [convert(entry.created_at), entry] }.sort! {|a, b|  b[0] <=> a[0] }
+    end
+
+    def convert(timestamp)
+      timezone = TZInfo::Timezone.get('Europe/Berlin')
+      timezone.utc_to_local(timestamp.to_datetime)
+    end
+
+    def remove_instagram_duplicates!(timeline)
+      timeline.each_with_index do |(_, entry), index|
+        timeline.delete_at(index) if entry.is_a?(Twitter::Tweet) && entry.source.include?('Instagram')
+      end
     end
   end
 end
