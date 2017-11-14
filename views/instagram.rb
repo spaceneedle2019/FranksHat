1062bac558db760cc32f43a4bbef3c690968dd40
Ronny Kraemer <mail@rk82.net>
2017-11-14 01:07:48 +0100

    Refactor Views:Instagram methods
    + Add nickname url matching

--- views/instagram.rb
+++ views/instagram.rb
@@ -7,15 +7,9 @@ module Views
         message_array = message.split(' ')
         message_array.map! do |word|
           if word.include?(Views::HASHMARK)
-            if word.end_with?(Views::POINT)
-              word.delete!(Views::POINT)
-              "#{url_for(word)}#{Views::POINT}"
-            elsif word.end_with?(Views::COMMA)
-              word.delete!(Views::COMMA)
-              "#{url_for(word)}#{Views::COMMA}"
-            else
-              url_for(word)
-            end
+            populate_tag_url(word)
+          elsif word.start_with?(Views::AT_SIGN)
+            "<a href='https://www.instagram.com/#{word.delete(Views::AT_SIGN)}'>#{word}</a>"
           else
             word
           end
@@ -24,6 +18,16 @@ module Views
       end
 
       private
+
+      def populate_tag_url(word)
+        if word.end_with?(Views::POINT)
+          "#{url_for(word.delete(Views::POINT))}#{Views::POINT}"
+        elsif word.end_with?(Views::COMMA)
+          "#{url_for(word.delete(Views::COMMA))}#{Views::COMMA}"
+        else
+          url_for(word)
+        end
+      end
 
       def url_for(tag)
         "<a href='https://www.instagram.com/explore/tags/#{tag.delete(Views::HASHMARK)}/'>#{tag}</a>"
