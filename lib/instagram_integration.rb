4766fac60e29c6e6736ed4d8b888e6cbcf57a91a
Ronny Kraemer <mail@rk82.net>
2018-03-16 22:01:02 +0100

    Repair requesting Instagram
    Because of changed JSON output. Instagram is now using GraphQL

--- lib/instagram_integration.rb
+++ lib/instagram_integration.rb
@@ -3,28 +3,29 @@ require 'uri'
 
 class InstagramIntegration
   def media
-    media_object.nodes.map do |item|
-      code = item.code
-      source = item.thumbnail_resources[2].src
-      created_at = DateTime.strptime(item.date.to_s, '%s')
-      image_link = "<a href='https://www.instagram.com/p/#{code}/'><img alt='#{code}'src='#{source}'></a>"
-      OpenStruct.new(:created_at => created_at, :text => item.caption, :image_link => image_link)
+    edges_array = response_object.graphql.user.edge_owner_to_timeline_media.edges
+    edges_array.map do |edge|
+      item = edge.node
+      OpenStruct.new(
+          created_at: DateTime.strptime(item.taken_at_timestamp.to_s, '%s'),
+          text: item.edge_media_to_caption.edges[0].node.text,
+          image_link: image_link(item.shortcode, item.thumbnail_resources[2].src)
+      )
     end
   end
 
   private
 
-  def media_object
+  def response_object
     response = Net::HTTP.get_response(url)
-    JSON.parse(response.body, object_class: OpenStruct).user.media if response.code.to_i == 200
-  end
-
-  def user_name
-    ENV['INSTAGRAM_USERNAME']
+    JSON.parse(response.body, object_class: OpenStruct) if response.code.to_i == 200
   end
 
   def url
-    URI.parse("https://www.instagram.com/#{user_name}/?__a=1")
+    URI.parse("https://www.instagram.com/#{ENV['INSTAGRAM_USERNAME']}/?__a=1")
   end
 
+  def image_link(code, source)
+    "<a href='https://www.instagram.com/p/#{code}/'><img alt='#{code}'src='#{source}'></a>"
+  end
 end
